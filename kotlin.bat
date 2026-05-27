704a17a5c49a7b688901b4c7c28808da7c8643d8
Suresh G <sureshg.g@gmail.com>
2026-05-26 22:39:56 -0700

    chore: fix mavenCentral publishing and Kotlin toolchain update

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-3982
+set kotlin_cli_version=0.12.0-dev-3986
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=42aa501169bbae7c441a783dad19db3f1dfe46059df4d63e03dd890cdfa12886
+set kotlin_cli_sha256=aab9e2fe08a73beea8562ec7a810d0113baa6e906c65beff4eaabae1339932ae
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
