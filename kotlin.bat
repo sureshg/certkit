4dadb28e73952accd368fd9b07e512825098d008
Suresh G <sureshg.g@gmail.com>
2026-06-11 22:12:06 -0700

    chore: update Kotlin toolchain

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4035
+set kotlin_cli_version=0.12.0-dev-4042
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=6dac646e607325e16a4ab9d0bb58418dc0090106420458052081ee92c7ba13c0
+set kotlin_cli_sha256=df9f820205bafb7946d77e5679e152bcd1834eb5c689be5ad6801a141062f6dc
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
